436356c324ba96cd658d371f3c56310b46a7b68a
Fernando Dornelles Rocha <fe.dornellesrocha@outlook.com>
2025-03-20 20:43:16 -0300

    feat: :sparkles: add tansckquery to project

--- src/app/pokemon/[id]/layout.tsx
+++ src/app/pokemon/[id]/layout.tsx
@@ -1,9 +1,7 @@
-import { queryClient } from "@/utils/client";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { QueryClientProvider } from "@tanstack/react-query";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -17,11 +15,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <html lang="pt-br">
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-        <body className="--font-roboto">{children}</body>
-      </html>
-    </QueryClientProvider>
+    <html lang="pt-br">
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
+      <body className="--font-roboto">{children}</body>
+    </html>
   );
 }
---
+++ src/app/pokemon/[id]/page.tsx
@@ -0,0 +1,6 @@
+"use client";
+import { Button } from "@mui/material";
+
+export default function Home() {
+  return <Button>teste</Button>;
+}
